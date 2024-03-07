78da24360acb71b877997b7ede1d5f359b869a0d
erwan.lemattre <erwan.lemattre@universite-paris-saclay.fr>
2024-03-08 00:24:06 +0100

    Continue typechecker

--- typechecker.ml
+++ typechecker.ml
@@ -1,4 +1,11 @@
-open Kawa
+open Imp
+
+type types =
+  | TInt
+  | TBool
+  | TArray
+  | TUndef
+  | TNull
 
 exception Error of string
 let error s = raise (Error s)
@@ -10,19 +17,26 @@ module Env = Map.Make(String)
 type tenv = typ Env.t
 
 let add_env l tenv =
-  List.fold_left (fun env (x, t) -> Env.add x t env) tenv l
+  match l with
+  | (s, r, _) :: ll -> List.fold_left (fun env (s, Some e, _) -> Env.add s e env) tenv l
+  | s :: ll         -> List.fold_left (fun env s -> Env.add s TNull env) tenv l
+  | _ -> tenv
 
 let typecheck_prog p =
   let tenv = add_env p.globals Env.empty in
-  let cls = List.fold_left (fun env o -> Env.add o.class_name o env) Env.empty p.classes in
+  let func =
+    List.fold_left (fun env f -> Env.add f.name f env) Env.empty p.functions in
+
 
   let rec check e typ tenv =
     let typ_e = type_expr e tenv in
     if typ_e <> typ then type_error typ_e typ
 
   and type_expr e tenv = match e with
+    | Null   -> TNull
     | Int i  -> TInt
     | Bool _ -> TBool
+    | Var s  -> TUndef
     (* Binop INT *)
     | Binop(Add, e1, e2) | Binop(Sub, e1, e2) | Binop(Mul, e1, e2) 
     | Binop(Div, e1, e2) | Binop(Rem, e1, e2) -> check e1 TInt tenv; check e2 TInt tenv; TInt
@@ -35,17 +49,22 @@ let typecheck_prog p =
     | Unop(Opp, e)       -> check e TInt tenv; TInt
     | Unop(Not, e)       -> check e TBool tenv; TBool
     
-    | 
+    | Call (s, l)    -> TUndef
+
+    | Array l        -> TArray
+    | GetArr (a, i)  -> TUndef
+    | Continuation   -> TUndef
                             
   in
 
   let rec check_instr i tenv = match i with
-    | Print e -> () (* Accepte tous les types *)
-    | If(e, i1, i2) -> check e TBool tenv
-    | While(e, s)   -> check e TBool tenv
-    | Expr(e)       -> ()
-    | Return(e)     -> ()
-    | Set(m, e)     -> ()  
+    | Print (e, _) -> () (* Accepte tous les types *)
+    | If(e, i1, i2, _) -> check e TBool tenv
+    | While(e, s, _)   -> check e TBool tenv
+    | Expr(e, _)       -> ()
+    | Return(e, _)     -> ()
+    | Set(m, e, _)     -> ()  
+    | SetArr (e1, e2, e3, _) -> ()
   and check_seq s tenv =
     List.iter (fun i -> check_instr i tenv) s
   in
